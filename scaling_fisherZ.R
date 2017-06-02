4e6c9e9d07c41773314df3544c36a4068c8415d6
ajrominger <ajrominger@gmail.com>
2017-06-02 17:04:00 -0600

    got ready to run across all data sets

--- scaling_fisherZ.R
+++ scaling_fisherZ.R
@@ -12,21 +12,9 @@ source('~/Dropbox/Research/meteR/R/sar_helper_funs.R')
 ## wd storing the data
 dataWD <- '~/Dropbox/Research/data/stri'
 
-## loop over data, computing z scaling for all
-allZ <- lapply(list.files(dataWD), function(f) {
-    ## read in data
-    x <- read.csv(file.path(dataWD, list.files(dataWD)[2]), as.is = TRUE)
-    
-    ## get most recent census
-    x <- x[x$year == max(x$year), ]
-    
-    ## get data.frame of z value scaling
-    return(scaleZ(x))
-})
 
 ## function to take plot data, divide it up across scales, and caluclate
 ## summaries of z-values across scale
-
 scaleZ <- function(x) {
     ## make raster object for plot
     r <- raster(ncols = ceiling(max(x$x)), nrows = ceiling(max(x$y)), 
@@ -41,19 +29,19 @@ scaleZ <- function(x) {
         if(s == 1) {
             abund <- tapply(x$count, x$spp, sum)
             thisSAD <- sad(abund, 'fish', keepData = TRUE)
-            
-            return(c(zMean = logLikZ(thisSAD)$z, zCI = rep(NA, 2), 
+
+            return(c(zMean = logLikZ(thisSAD)$z, zCI = rep(NA, 2),
                      zPermMean = NA, zPermCI = rep(NA, 2)))
         } else {
             ## rescale
             res(r) <- scales[s, ]
-            
+
             ## within a scale, loop over cells to calcuate SAD z-values within each
-            
+
             cells <- cellFromXY(r, xy = x[, c('x', 'y')])
             # cells[is.na(cells)] <- -1 # avoid issues with NA when matching below
             cellsPerm <- sample(cells) # permuted cells
-            
+
             # zz <- mclapply(unique(cells[cells > 0]), mc.cores = 6, function(i) {
             zz <- mclapply(unique(cells), mc.cores = 6, function(i) {
                 newx <- x[cells == i, ]
@@ -63,21 +51,21 @@ scaleZ <- function(x) {
                 zPerm <- calcZ(x, cellsPerm, i)
                 return(c(z, zPerm))
             })
-            
+
             zz <- do.call(rbind, zz)
-            
-            
+
+
             ## return averaged z vales
-            return(c(zMean = mean(zz[, 1]), zCI = quantile(zz[, 1], c(0, 0.95), 
-                                                           names = FALSE), 
-                     zPermMean = mean(zz[, 2]), zPermCI = quantile(zz[, 2], c(0, 0.95), 
+            return(c(zMean = mean(zz[, 1]), zCI = quantile(zz[, 1], c(0, 0.95),
+                                                           names = FALSE),
+                     zPermMean = mean(zz[, 2]), zPermCI = quantile(zz[, 2], c(0, 0.95),
                                                                    names = FALSE)))
         }
     })
-    
+
     ## return scale as area, and z-value summary
     out <- data.frame(scale = scales[, 1] * scales[, 2], do.call(rbind, out))
-    
+
     return(out)
 }
 
@@ -97,15 +85,13 @@ determineScale <- function(r) {
     ymx <- ymax(r)
     
     ## target minimum area
-    targetArea <- 16
+    targetArea <- 10
     
     ## number of halvings allowed by dimensions of plot
     n <- floor(log(targetArea / (xmx * ymx)) / (-2 * log(2)))
     
     return(cbind(xmx * 2^-(0:n), ymx * 2^-(0:n)))
 }
-
-foo <- scaleZ(x)
 
 ## function to graph one plot's worth of scaling
 plotScaleZ <- function(z, ...) {
@@ -126,5 +112,19 @@ plotScaleZ <- function(z, ...) {
          pch = 16, cex = cex, ...)
 }
 
-plotScaleZ(foo)
+## loop over data, computing z scaling for all
+allZ <- lapply(list.files(dataWD), function(f) {
+    ## read in data
+    x <- read.csv(file.path(dataWD, f), as.is = TRUE)
+    
+    ## get most recent census
+    x <- x[x$year == max(x$year), ]
+    
+    ## get data.frame of z value scaling
+    out <- cbind(plot = gsub('.csv', '', f), scaleZ(x))
+    return(out)
+})
 
+allZ <- do.call(rbind, allZ)
+
+write.csv(allZ, 'scaling_fisherZ.csv', row.names = FALSE)
