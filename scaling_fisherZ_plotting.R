7392297100855411a3306b2b832d4bba8f767c1d
ajrominger <ajrominger@gmail.com>
2017-06-09 20:51:20 -0600

    updated plotting to be more clean

--- scaling_fisherZ_plotting.R
+++ scaling_fisherZ_plotting.R
@@ -16,7 +16,8 @@ plotScaleZ <- function(z, ...) {
          panel.first = {
              lines(jitterScale, z$zPermMean, col = permCol, lwd = lwd)
              segments(x0 = jitterScale, y0 = z$zPermCI1, y1 = z$zPermCI2, col = permCol)
-             points(jitterScale, z$zPermMean, pch = 21, col = permCol, bg = 'white', cex = cex)
+             points(jitterScale, z$zPermMean, 
+                    pch = 21, col = permCol, bg = 'white', cex = cex)
              lines(z$scale, z$zMean, lwd = lwd)
              segments(x0 = z$scale, y0 = z$zCI1, y1 = z$zCI2)
          },
@@ -27,10 +28,19 @@ plotScaleZ <- function(z, ...) {
 
 
 sortedPlots <- names(sort(tapply(allZ$scale, allZ$plot, max), decreasing = TRUE))
-par(mfcol = c(length(sortedPlots), 1), mar = c(0, 3, 0, 2) + 0.5, oma = c(3.5, 0, 0, 0))
+par(mfcol = c(length(sortedPlots), 1), mar = c(0, 0, 0, 2) + 0.5, 
+    oma = c(3.5, 3.5, 0, 0), mgp = c(2, 0.75, 0))
 for(i in 1:length(sortedPlots)) {
     plotScaleZ(allZ[allZ$plot == sortedPlots[i],], 
-               xaxt = 'n', log = 'xy',
-               xlim = range(allZ$scale), ylim = c(1e-05, 500))
-    if(i == length(sortedPlots)) logAxis(1)
+               xaxt = 'n', yaxt = 'n', log = 'xy',
+               xlim = range(allZ$scale), ylim = c(1e-05, 500), 
+               xlab = '', ylab = '')
+    axis(2, at = 10^pretty(log(c(1e-05, 500), 10)), 
+         labels = sapply(pretty(log(c(1e-05, 500), 10)), 
+                         function(p) eval(substitute(expression(10^p), list(p = p)))))
+    if(i == length(sortedPlots)) logAxis(1, expLab = TRUE)
+    mtext(sortedPlots[i], side = 4, line = 1)
 }
+
+mtext(expression('Scale'~(m^2)), side = 1, outer = TRUE, line = 2)
+mtext(expression(z^2-'value'), side = 2, outer = TRUE, line = 2)
