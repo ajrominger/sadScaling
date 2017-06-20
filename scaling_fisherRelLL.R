72945870ab21cd19c3563561d0c2ca7f1f94dc50
ajrominger <ajrominger@gmail.com>
2017-06-20 16:01:16 -0600

    script to calculate relative logLik instead of z^2

---
+++ scaling_fisherRelLL.R
@@ -0,0 +1,95 @@
+library(pika)
+library(socorro)
+library(raster)
+
+setwd('~/Dropbox/Research/sadScaling')
+
+source('../happySAD/sampleTransform.R')
+
+bci <- read.csv('../data/stri/BCIS.csv', as.is = TRUE)
+bci <- bci[bci$year == max(bci$year), ]
+
+
+## function to take plot data, divide it up across scales, and caluclate
+## summaries of relative log likelihoods across scale
+scaleRelLL <- function(x) {
+    ## make raster object for plot
+    r <- raster(ncols = ceiling(max(x$x)), nrows = ceiling(max(x$y)), 
+                xmn = 0, xmx = ceiling(max(x$x)), 
+                ymn = 0, ymx = ceiling(max(x$y)))
+    
+    ## determine range of scales
+    scales <- determineScale(r)
+    
+    ## loop over scales, cutting data by re-scaled raster and calculating SAD z-values
+    out <- lapply(1:nrow(scales), function(s) {
+        if(s == 1) {
+            # abund <- tapply(x$count, x$spp, sum)
+            # thisSAD <- sad(abund, 'fish', keepData = TRUE)
+            # 
+            # return(c(zMean = logLikZ(thisSAD)$z, zCI = rep(NA, 2),
+            #          zPermMean = NA, zPermCI = rep(NA, 2)))
+            return(NULL)
+        } else {
+            ## rescale
+            res(r) <- scales[s, ]
+            
+            ## within a scale, loop over cells to calcuate SAD z-values within each
+            
+            cells <- cellFromXY(r, xy = x[, c('x', 'y')])
+            # cells[is.na(cells)] <- -1 # avoid issues with NA when matching below
+            cellsPerm <- sample(cells) # permuted cells
+            
+            ll <- lapply(unique(cells), FUN = function(i) {
+                browser()
+                ll <- calcLL(x, cells, i)
+                zPerm <- calcZ(x, cellsPerm, i)
+                return(c(z, zPerm))
+            })
+            
+            zz <- do.call(rbind, zz)
+            
+            
+            ## return averaged z vales
+            return(c(zMean = mean(zz[, 1]), zCI = quantile(zz[, 1], c(0, 0.95),
+                                                           names = FALSE),
+                     zPermMean = mean(zz[, 2]), zPermCI = quantile(zz[, 2], c(0, 0.95),
+                                                                   names = FALSE)))
+        }
+    })
+    
+    ## return scale as area, and z-value summary
+    out <- data.frame(scale = scales[, 1] * scales[, 2], do.call(rbind, out))
+    
+    return(out)
+}
+
+## helper function to subset data by cell and calculate log liks
+calcLL <- function(x, cells, i) {
+    newx <- x[cells == i, ]
+    abund <- tapply(newx$count, newx$spp, sum)
+    llF <- logLik(sad(abund, 'fish'))
+    llST <- sum(sampTrans(abund, sad(tapply(x$count, x$spp, sum)), nrow(newx), 
+                          include0 = FALSE, log = TRUE))
+    
+    return(c(llF, llST))
+}
+
+
+## helper function to determine scale for raster of plot
+## note: scale (or resolution) must be a multiple of each dimension of the plot
+## to work easily, so just scale each side independently
+determineScale <- function(r) {
+    xmx <- xmax(r)
+    ymx <- ymax(r)
+    
+    ## target minimum area
+    targetArea <- 10
+    
+    ## number of halvings allowed by dimensions of plot
+    n <- floor(log(targetArea / (xmx * ymx)) / (-2 * log(2)))
+    
+    return(cbind(xmx * 2^-(0:n), ymx * 2^-(0:n)))
+}
+
+scaleRelLL(bci)
