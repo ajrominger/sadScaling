bd6ec0ad6f715b30fed4a36f4ded46248cc23b14
ajrominger <ajrominger@gmail.com>
2017-06-22 14:27:20 -0600

    removed plotting

--- scaling_fisherSingletons.R
+++ scaling_fisherSingletons.R
@@ -11,6 +11,9 @@ source('scaling_helpers.R')
 bci <- read.csv('../data/stri/BCIS.csv', as.is = TRUE)
 bci <- bci[bci$year == max(bci$year), ]
 
+paso <- read.csv('../data/stri/PASO.csv', as.is = TRUE)
+paso <- paso[paso$year == max(paso$year), ]
+
 ## function to calculate singletons (obs and theory)
 singleton <- function(x, newx) {
     abund <- tapply(newx$count, newx$spp, sum)
@@ -30,43 +33,5 @@ singScaleSumm <- ddply(singScale, 'scale', function(x) {
     return(out)
 })
 
-par(mfrow = c(1, 2), mar = c(0, 0, 2, 0) + 0.5, 
-    oma = c(3, 3, 1, 1), mgp = c(2, 0.75, 0))
-col <- quantCol(singScaleSumm$scale, pal = hsv(c(0.12, 0.6, 1), c(0.8, 1, 1), 
-                                               c(0.8, 0.8, 0.7)), 
-                trans = 'log')
-plot(singScaleSumm$n1fish, singScaleSumm$n1obs, pch = 16,
-     col = col, panel.first = {
-         lines(singScaleSumm$n1fish, singScaleSumm$n1obs, lty = 2, col = 'gray')
-         segments(x0 = singScaleSumm$n1fish, 
-                  y0 = singScaleSumm$n1obs.ci1, y1 = singScaleSumm$n1obs.ci2, 
-                  col = col)
-         segments(x0 = singScaleSumm$n1fish.ci1, x1 = singScaleSumm$n1fish.ci2, 
-                  y0 = singScaleSumm$n1obs, 
-                  col = col)
-     }, 
-     xlim = range(singScaleSumm[, grepl('n1fish', names(singScaleSumm))]), 
-     ylim = range(singScaleSumm[, grepl('n1obs', names(singScaleSumm))]))
-abline(0, 1)
-mtext('Spatial subset', side = 3, outer = FALSE, line = 0)
-
-plot(singScaleSumm$perm.n1fish, singScaleSumm$perm.n1obs, pch = 16,
-     col = col, 
-     panel.first = {
-         lines(singScaleSumm$perm.n1fish, singScaleSumm$perm.n1obs, lty = 2, col = 'gray')
-         segments(x0 = singScaleSumm$perm.n1fish, 
-                  y0 = singScaleSumm$perm.n1obs.ci1, y1 = singScaleSumm$perm.n1obs.ci2, 
-                  col = col)
-         segments(x0 = singScaleSumm$perm.n1fish.ci1, x1 = singScaleSumm$perm.n1fish.ci2, 
-                  y0 = singScaleSumm$perm.n1obs, 
-                  col = col)
-     }, 
-     yaxt = 'n',
-     xlim = range(singScaleSumm[, grepl('perm.n1fish', names(singScaleSumm))]), 
-     ylim = range(singScaleSumm[, grepl('perm.n1obs', names(singScaleSumm))]))
-abline(0, 1)
-
-mtext('Random subset', side = 3, outer = FALSE, line = 0)
-
-mtext('Logseries-predicted singletons', side = 1, outer = TRUE, line = 1.5)
-mtext('Observed singletons', side = 2, outer = TRUE, line = 1.5)
+## write out
+write.csv(singScaleSumm, 'scaling_fisherSingletons.csv', row.names = FALSE)
