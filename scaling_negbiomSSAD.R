8f73c957016583b165b60fd51906573379527c0e
ajrominger <ajrominger@gmail.com>
2017-06-29 12:10:59 -0600

    got script ready for background run

--- scaling_negbiomSSAD.R
+++ scaling_negbiomSSAD.R
@@ -18,21 +18,6 @@ bci <- bci[bci$year == max(bci$year), ]
 paso <- read.csv('../data/stri/PASO.csv', as.is = TRUE)
 paso <- paso[paso$year == max(paso$year), ]
 
-foo <- replicate(500, {
-    bci1 <- bci[sample(nrow(bci), 10000), ]
-    sum(bci1$spp == 'CROTBI')
-})
-
-foofit <- fitdistr(foo, 'negative binomial')
-plot(simpECDF(foo))
-curve(pnbinom(x, size = foofit$estimate[1], mu = foofit$estimate[2]), add = TRUE, col = 'red')
-curve(ppois(x, mean(foo)), add = TRUE, col = 'blue')
-
-## fit negbinom and pois and compare logLik of the two
-## figure out sampTrans type thing for neg binom
-## compare KL distance of best fit nebginom and pois
-
-
 
 ## function to take plot data, divide it up across scales, and caluclate
 ## metrics across scales
@@ -40,7 +25,7 @@ curve(ppois(x, mean(foo)), add = TRUE, col = 'blue')
 #' @param fun is a function returning the metric of interest
 #' @param perm is a logical indicating whether to compute the metric on permuted cells
 
-scaleNegBinom <- function(x, perm = TRUE) {
+scaleNegBinom <- function(x) {
     ## make raster object for plot
     r <- raster(ncols = ceiling(max(x$x)), nrows = ceiling(max(x$y)), 
                 xmn = 0, xmx = ceiling(max(x$x)), 
@@ -53,44 +38,23 @@ scaleNegBinom <- function(x, perm = TRUE) {
     scales <- scales[-(1:4), ]
     
     ## loop over scales, cutting data by re-scaled raster and calculating SAD z-values
-    out <- lapply(1:nrow(scales), FUN = function(s) {
+    out <- mclapply(1:nrow(scales), mc.cores = 6, FUN = function(s) {
+    # out <- lapply(1:nrow(scales), FUN = function(s) {
         ## rescale
         res(r) <- scales[s, ]
         
         ## make community matrices for permuted and non-permuted cells
         cells <- cellFromXY(r, xy = x[, c('x', 'y')])
         cellsPerm <- sample(cells) # permuted cells
+        
         mat <- tidy2mat(cells, x$spp, x$count)
-        if(perm) permMat <- tidy2mat(cells, x$spp, x$count)
+        permMat <- tidy2mat(cellsPerm, x$spp, x$count)
         
         sppOut <- lapply(1:ncol(mat), function(i) {
-            ## get distrib params
-            mu <- mean(foo)
-            size <- mu^2 / (var(foo) - mu)
+            m <- calcAtScale(mat, i)
+            permM <- calcAtScale(permMat, i)
             
-            nbPar <- c(k = mean(mat[, i]) / (var(mat[, i]) - mean(mat[, i])), 
-                       mu = mean(mat[, i]))
-            pPar <- mean(mat[, i])
-            
-            ## calculate likelihoods
-            nbLL <- sum(dnbinom(mat[, i], nbPar[1], mu = nbPar[2], log = TRUE))
-            pLL <- sum(dpois(mat[, i], pPar, log = TRUE))
-            
-            ## calculate z2 for negbinom
-            p0 <- dnbinom(0:10^6, size = nbPar[1], mu = nbPar[2], log = TRUE)
-            p0 <- p0[is.finite(p0)]
-            n <- nrow(mat)
-            m <- sum(p0 * exp(p0)) * n
-            v <- sum((m/n - p0)^2 * exp(p0)) * n
-            nbZ <- ((nbLL - m) / sqrt(v))^2
-            
-            ## calculate KL dist for neg binom and pois
-            q0 <- dpois(seq(0, length.out = length(p0)), pPar, log = TRUE)
-            p0 <- p0[is.finite(q0)]
-            q0 <- q0[is.finite(q0)]
-            kl <- sum(exp(p0) * (p0 - q0))
-            
-            return(c(nbLL = nbLL, pLL = pLL, nbZ = nbZ, nbPar = nbPar, kl = kl))
+            return(c(m, perm = permM))
         })
         
         sppOut <- do.call(rbind, sppOut)
@@ -132,7 +96,48 @@ determineScale <- function(r) {
     }
 }
 
+calcAtScale <- function(mat, i) {
+    ## get distrib params
+    matMu <- mean(mat[, i])
+    matVar <- var(mat[, i])
+    if(matVar <= matMu) {
+        nbPar <- try(fitdistr(mat[, i], 'negative binomial')$estimate, silent = TRUE)
+        if(class(nbPar) == 'try-error') nbPar <- c(NA, NA)
+    } else {
+        nbPar <- c(k = mean(mat[, i]) / (var(mat[, i]) - mean(mat[, i])), 
+                   mu = mean(mat[, i]))
+    }
+    
+    pPar <- mean(mat[, i])
+    
+    ## calculate likelihoods
+    nbLL <- sum(dnbinom(mat[, i], nbPar[1], mu = nbPar[2], log = TRUE))
+    pLL <- sum(dpois(mat[, i], pPar, log = TRUE))
+    
+    ## calculate z2 for negbinom
+    nmax <- qnbinom(1-.Machine$double.eps^0.9, size = nbPar[1], mu = nbPar[2])*50
+    if(!is.integer(nmax)) nmax <- 1
+    p0 <- dnbinom(0:nmax, size = nbPar[1], mu = nbPar[2], log = TRUE)
+    p0 <- p0[is.finite(p0)]
+    n <- nrow(mat)
+    m <- sum(p0 * exp(p0)) * n
+    v <- sum((m/n - p0)^2 * exp(p0)) * n
+    nbZ <- ((nbLL - m) / sqrt(v))^2
+    
+    ## calculate KL dist for neg binom and pois
+    q0 <- dpois(seq(0, length.out = length(p0)), pPar, log = TRUE)
+    p0 <- p0[is.finite(q0)]
+    q0 <- q0[is.finite(q0)]
+    kl <- sum(exp(p0) * (p0 - q0))
+    
+    return(c(nbLL = nbLL, pLL = pLL, nbZ = nbZ, nbPar = nbPar, kl = kl))
+}
 
 ## calculate singletons across scales
 negbBCI <- scaleNegBinom(bci)
 negbPASO <- scaleNegBinom(paso)
+
+negbScale <- rbind(cbind(site = 'BCI', negbBCI), cbind(site = 'PASO', negbPASO))
+
+write.csv(negbScale, file = 'scaling_negbiomSSAD.csv', row.names = FALSE)
+
